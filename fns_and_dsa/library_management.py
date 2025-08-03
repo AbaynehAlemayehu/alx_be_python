e491b3f4e6a519717ad7918736401c8e29551ad1
AbaynehAlemayehu <abneh21@gmail.com>
2025-08-03 19:41:11 +0300

    add

--- fns_and_dsa/library_management.py
+++ fns_and_dsa/library_management.py
@@ -16,4 +16,37 @@ class Book:
 
     def is_available(self):
         """Check if the book is available for checkout."""
-        return not self
+        return not self._is_checked_out
+
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        """Add a Book instance to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by title if available."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return
+        print(f"'{title}' is not available for checkout.")
+
+    def return_book(self, title):
+        """Return a book by title."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return
+        print(f"'{title}' was not checked out or does not exist.")
+
+    def list_available_books(self):
+        """List all books that are currently available."""
+        available_books = [book for book in self._books if book.is_available()]
+        if not available_books:
+            print("No books available.")
+        for book in available_books:
+            print(f"{book.title} by {book.author}")
