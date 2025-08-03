d546cd8127d3168fa1e698e97640b8ecd56c7a98
AbaynehAlemayehu <abneh21@gmail.com>
2025-08-03 19:37:35 +0300

    add

---
+++ fns_and_dsa/library_management.py
@@ -0,0 +1,19 @@
+# library_management.py
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        self._is_checked_out = True
+
+    def return_book(self):
+        """Mark the book as returned."""
+        self._is_checked_out = False
+
+    def is_available(self):
+        """Check if the book is available for checkout."""
+        return not self
